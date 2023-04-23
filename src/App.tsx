596c0b5dbece2ca74bb2286e3ce439c295c1e8f5
Thiago Ferreira <thiagorocha503@gmail.com>
2023-04-23 10:15:29 -0300

    🎉 Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,62 @@
+import { useState, useEffect } from "react";
+import "./App.css";
+const API_URL: string = "https://dummyjson.com/quotes/random";
+export default function App() {
+    // states
+    const [quote, setQuote] = useState<{ text: string; author: string }>({
+        text: "",
+        author: "",
+    });
+
+    // component did mounted
+    useEffect(() => {
+        fetchQuote();
+    }, []);
+
+    // fetch quote
+    const fetchQuote = async () => {
+        try {
+            const res: Response = await fetch(API_URL);
+            const data = await res.json();
+            setQuote({
+                text: data.quote,
+                author: data.author,
+            });
+        } catch (e) {
+            if (e instanceof Error) {
+                console.error(e.message);
+                return;
+            }
+            console.error(e);
+        }
+    };
+
+    return (
+        <div className="card p-2" id="quote-box">
+            <div className="card-body">
+                <p id="text">❝{quote.text}❞</p>
+                <p className="text-end" id="author">
+                    — {quote.author}
+                </p>
+                <div className=" mt-3 d-flex justify-content-end">
+                    <a
+                        className="btn btn-outline-primary me-3"
+                        href={`https://twitter.com/intent/tweet?text=“${quote.text}” — ${quote.author}`}
+                        target="_blank"
+                        rel="noreferrer"
+                        id="tweet-quote"
+                    >
+                        Tweet quote
+                    </a>
+                    <button
+                        className="btn btn-primary"
+                        id="new-quote"
+                        onClick={() => fetchQuote()}
+                    >
+                        New quote
+                    </button>
+                </div>
+            </div>
+        </div>
+    );
+}
